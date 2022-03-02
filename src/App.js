4268691008b77064fad3f2f90bec84375703eb20
Hoang Huy Nguyen <hoanghuynguyen.info@gmail.com>
2022-03-02 20:16:01 +0700

    C1 - Upload source code first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react'
+import Content from './Content'
+
+
+// function App() {
+//   const [job, setJob] = useState('')
+//   const [jobs, setJobs] = useState(() => {
+//     const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+//     console.log('value')
+//     return storageJobs ?? []
+//   })
+//   const handleSubmit = () => {
+//     setJobs(prev => {
+//       const newJobs =[...prev, job]
+//       localStorage.setItem('jobs', JSON.stringify(newJobs))
+//       return newJobs
+//     })
+//     setJob('')
+//   }
+
+//   return (
+//     <div style={{padding: 32}}>
+//       <input 
+//       value={job}
+//       onChange={e => setJob(e.target.value)}
+//       />
+//       <button onClick={handleSubmit}>Add</button>
+
+//       <ul>
+//         {jobs.map((item, index) => 
+//           <li key={index}>{item}</li>
+//         )}
+//       </ul>
+//     </div>
+//   )
+// }
 
 function App() {
+  const [show, setShow] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content />}
     </div>
-  );
+  )
 }
 
 export default App;
